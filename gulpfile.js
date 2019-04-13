15208fdb7397ff2ac5f2132c46f8d346d64774ec
Nikita <n.kuznetsov@pitech.ru>
2019-04-13 12:56:12 +0300

    update 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,21 @@ var gulp                  = require('gulp'),
     browserSync           = require('browser-sync'),
     concatCss             = require('gulp-concat-css'),
     autoprefixer          = require('gulp-autoprefixer'),
-    clean                 = require('gulp-clean'),
     imagemin              = require('gulp-imagemin'),
-    fsync                 = require('gulp-files-sync'),
     uglify                = require('gulp-uglify'),
     concat                = require('gulp-concat');
 
 gulp.task('sass', function() {
-  gulp.src('src/app/style/block/**/*.scss')
+  gulp.src('src/app/style/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(concatCss("style.css"))
+    .pipe(concatCss('style.css'))
     .pipe(gulp.dest('src/app/style/css'));
 
-  gulp.src('src/app/style/plugin/**/*.css')
-    .pipe(concatCss("plugin.css"))
-    .pipe(gulp.dest('src/app/style/css/'));
-
-  gulp.src('src/app/fonts/*.css')
-    .pipe(concatCss("fonts.css"))
-    .pipe(gulp.dest('src/app/style/css/'));
-
-  gulp.watch('src/app/fonts/*.css', gulp.series('sass'));
-  gulp.watch('src/app/style/block/**/*.scss', gulp.series('sass'));
-  gulp.watch('src/app/style/plugin/**/*.css', gulp.series('sass'));
+  gulp.watch('src/app/style/**/*.scss', gulp.series('sass'));
 });
 
 gulp.task('script', function() {
@@ -38,7 +26,7 @@ gulp.task('script', function() {
     .pipe(uglify())
     .pipe(gulp.dest('src/app/script'));
 
-  gulp.watch('src/app/script/**/*.js', gulp.series('script'));
+  gulp.watch('src/app/script/plugin/**/*.js', gulp.series('script'));
 });
 
 gulp.task('sync', function() {
@@ -49,12 +37,3 @@ gulp.task('sync', function() {
 });
 
 gulp.task('default', gulp.parallel('sync', 'sass', 'script'));
-
-gulp.task('default', gulp.parallel('sync', 'sass', 'script'));
-
-// gulp.task('media', function() {
-//   gulp.src('src/frontend/media/**/*')
-//     .pipe(imagemin())
-//     .pipe(gulp.dest('src/app/media/'));
-//   gulp.watch('src/frontend/media/**/*', gulp.series('cleare', 'media'));
-// });
