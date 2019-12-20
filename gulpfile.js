e6f0bb98d7fff60669aff6bd7a2cf142b1c50705
Nikita <n.kuznetsov@pitech.ru>
2019-12-20 19:08:12 +0300

    global fix 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,13 @@
 var browserSync = require('browser-sync'),
     gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    concatCss = require('gulp-concat-css'),
-    sass = require('gulp-sass'),
-    rigger = require('gulp-rigger'),
-    uglify = require('gulp-uglify'),
-    del = require('del');
-
-gulp.task('include', function () {
-  return gulp.src('src/template/*.html')
-    .pipe(rigger())
-    .pipe(gulp.dest('src/'))
-});
-
-gulp.task('clean', function () {
-  return del('src/*.html');
-});
+    autoprefixer = require('gulp-autoprefixer'),  // добавляем префиксы
+    concat = require('gulp-concat'),              // сборка файлов в один
+    concatCss = require('gulp-concat-css'),       // сборка файлов в один
+    sass = require('gulp-sass'),                  // препроцессор
+    uglify = require('gulp-uglify'),              // сжатие js
+    del = require('del'),                         // удаление
+    pug = require('gulp-pug'),                    // шаблонизатор HTML
+    prettify = require('gulp-html-prettify');     // форматирование HTML
 
 gulp.task('sass', function () {
   return gulp.src(['src/sass/core/*.scss', 'src/sass/plugin/**/*.scss', 'src/sass/block/**/*.scss'])
@@ -37,15 +28,91 @@ gulp.task('js', function () {
     .pipe(gulp.dest('src/js'));
 });
 
-gulp.task('sync', function () {
-  browserSync.init({
-    server: 'src'
-  });
-
-  gulp.watch(['src/template/**/*.html', 'src/js/*.js', 'src/media/', 'src/fonts/']).on('change', browserSync.reload);
-  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
-  gulp.watch('src/js/plugin/**/*.js', gulp.series('js'));
-  gulp.watch('src/template/**/*.html', gulp.series('clean', 'include'));
+gulp.task('pug', function () {
+  return gulp.src('src/template/pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('src/template'));
 });
 
-gulp.task('default', gulp.parallel('sync', 'sass', 'js', 'include'));
+gulp.task('prettify', function() {
+  gulp.src('src/template/*.html')
+    .pipe(prettify({
+      indent_char: ' ',
+      indent_size: 2
+    }))
+    .pipe(gulp.dest('src/template'))
+});
+
+gulp.task('del', function() {
+  return del('src/template/*.html');
+});
+
+gulp.task('sync', function () {
+  browserSync.init({
+    server: ['src', 'src/template']
+  });
+
+  gulp.watch(['src/template/*.html', 'src/js/*.js', 'src/media/', 'src/fonts/']).on('change', browserSync.reload);
+  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('src/js/plugin/**/*.js', gulp.series('js'));
+  gulp.watch('src/template/pug/**/*.pug', gulp.series('del', 'pug', 'prettify'));
+});
+
+gulp.task('default', gulp.parallel('sync', 'sass', 'js', 'pug'));
+
+// build
+
+gulp.task('sass-build', function () {
+  return gulp.src(['src/sass/core/*.scss', 'src/sass/plugin/**/*.scss', 'src/sass/block/**/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concatCss('style.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('js-build', function () {
+  return gulp.src(['src/js/*.js'], {
+    base: 'src'
+  })
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('fonts-build', function () {
+  return gulp.src(['src/fonts/**/*'], {
+    base: 'src'
+  })
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('pug-build', function () {
+  return gulp.src('src/template/pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build/template'));
+});
+
+gulp.task('prettify-build', function() {
+  gulp.src('build/template/*.html')
+    .pipe(prettify({
+      indent_char: ' ',
+      indent_size: 2
+    }))
+    .pipe(gulp.dest('build/template'))
+});
+
+gulp.task('del-build', function() {
+  return del('build');
+});
+
+gulp.task('prettify-build', function() {
+  gulp.src('src/template/*.html')
+    .pipe(prettify({
+      indent_char: ' ',
+      indent_size: 2
+    }))
+    .pipe(gulp.dest('build/template'))
+});
+
+gulp.task('build', gulp.series('del-build', 'sass-build', 'js', 'js-build', 'pug-build', 'prettify-build', 'fonts-build'));
